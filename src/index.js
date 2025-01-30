a8db58accb17323df4ff9f382535a5bd12ddb0c5
matejadb <92729211+matejadb@users.noreply.github.com>
2025-01-30 16:09:59 +0100

    Update goals

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import './styles.css';
 import { screenController } from './modules/screenController';
 
 screenController();
+
+//===========================================================
+// Next day goals
+//===========================================================
+// 1) New project creation
+// 2) Ensure a created task goes into the selected project
+// 3) Display tasks that are in a selected project
